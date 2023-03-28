02f50dfc19159393a048a18980e9c316ef4ec6e1
Affulti <Affulti@outlook.com>
2023-03-28 20:08:44 +0800

    String--KMP

--- String/String.h
+++ String/String.h
@@ -39,5 +39,59 @@ int Index(SString S, SString T){
 }
 
 //2. KMP算法
+void get_next(SString T, int next[]){
+    int i,j = 0;
+    next[1] = 0;
+    while(i < T.length){
+        if(j == 0 || T.ch[i] == T.ch[j]){
+            ++i;
+            ++j;
+            //若P(i) = P(j), 则next[j+1] = next[j] + 1
+            next[i] = j;
+        }else{
+            //否则令j = next[j],循环继续
+            j = next[j];
+        }
+    }
+}
+
+int Index_KMP(SString S, SString T, int next[]){
+    int i = 1, j = 1;
+    while(i <= S.length && j <= T.length){
+        if(j == 0 || S.ch[i] == T.ch[j]){
+            //继续比较后继字符
+            ++i;
+            ++j;
+        }else{
+            //模式串向右移动
+            j = next[j];
+        }
+        if( j > T.length){
+            //匹配成功
+            return i - T.length;
+        }else{
+            return 0;
+        }
+    }
+}
 
 //3. KMP算法的优化
+
+void get_nextval(SString T, int nextval[]){
+    int i,j = 0;
+    nextval[1] = 0;
+    while(i < T.length){
+        if(j == 0 || T.ch[i] == T.ch[j]){
+            ++i;
+            ++j;
+            if(T.ch[i] != T.ch[j]){
+                nextval[i] = j;
+            }else{
+                nextval[i] = nextval[j]; 
+            }
+        }else{
+            //否则令j = next[j],循环继续
+            j = nextval[j];
+        }
+    }    
+}
